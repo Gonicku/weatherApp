f0b58877097e9d96343dd7e29470ba24cecf4dc2
Duncan McDonald <duncanmcd384@gmail.com>
2024-03-01 15:22:09 -0500

    Hamza initial React work

---
+++ React/React/WEAT/weat/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="WEAT" style={{backgroundColor: 'black'}}>
+      <header className="WEAT-header">
+        <img src={'./wireframe.png'} className="WEAT-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
