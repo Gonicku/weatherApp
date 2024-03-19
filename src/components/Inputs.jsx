0ef8d248437dde88a583bf4f3055b85c9c72ce28
Harman Barpaga <harmanbarpaga1@gmail.com>
2024-03-18 22:05:10 -0400

    new commit

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import { UilSearch, UilLocationPoint  } from '@iconscout/react-unicons'
+
+
+
+function Inputs({setQuery, units, setUnits}) {
+  const [city, setCity] = useState("");
+  const handleSearchClick = () => {
+    if (city !== '') setQuery({q: city})  
+  }
+  return (
+    <div className="flex flex-row justify-center my-6">
+    <div className="flex flex-row  w-3/4 items-center justify-center space-x-4">
+        <input
+        value={city}
+        onChange={(e) => setCity(e.currentTarget.value)}
+        type="text" 
+        placeholder="Search for a city..."
+        className="text-xl font-light p-2
+        w-full shadow-xl focus:outline-none capitalize placeholder:lowercase" />
+        <UilSearch size ={20}  className="text-white cursor-pointer tansition ease-out hover:scale-125"
+        onClick={handleSearchClick}/>
+        <UilLocationPoint size ={20} className="text-white cursor-pointer tansition ease-out hover:scale-125" />
+     </div>
+     
+     <div className="flex flex-row w-1/4 items-center justify-center">
+      <button name="metric" className="text-xl text-white font-light tansition ease-out hover:scale-125" >°C</button>
+      <p className="text-xl text-white mx-1">|</p>
+      <button name="imperial" className="text-xl text-white font-light tansition ease-out hover:scale-125">°F</button>
+     </div>
+
+    </div>
+  )
+}
+
+export default Inputs
