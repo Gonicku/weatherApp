f2781a200bd840d512129bafb22c1c7c3619cb1b
Gonicku <gonicku@gmail.com>
2024-04-28 18:08:02 -0400

    Fixed the 4 tests

--- src/tests/HourlyForecast.test.jsx
+++ src/tests/HourlyForecast.test.jsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+//import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from '@testing-library/react';
 import HourlyForecast from "../components/HourlyForecast";
 import getFormattedWeatherData from "../components/services/weatherService";
 
@@ -10,54 +11,60 @@ describe(HourlyForecast, () => {
     })
 
     it("Initializes to the first page of weather data.", () => {
-        render(<HourlyForecast title="Hourly" items={weather} />)
+        //render(<HourlyForecast title="Hourly" items={weather} />)
+        render(<HourlyForecast title = "Hourly" items = {weather}/>);
         const pageElement = screen.getByTestId("page") // Not grabbing
-        expect(pageElement).not.toEqual(null)
-        const pageNumber = screen.getByTestId("page").value
+        //expect(pageElement).not.toEqual(null)
+        //const pageNumber = screen.getByTestId("page").value
+        const pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
         expect(pageNumber).toEqual(1)
     })
     it("Increments and does not pass the seventh page", () => {
-        render(<HourlyForecast title="Hourly" items={weather} />)
-        const pageNumber = screen.getByTestId("page").value  // Not grabbing
-        const incrementPage = screen.getByRole("button", { name: ">>" }) // Not grabbing - easy fix
+        render(<HourlyForecast title = "Hourly" items = {weather} />)
+        var pageNumber = Number(screen.getByTestId("page").getAttribute('value'))  // Not grabbing
+        const incrementPage = screen.getByRole("button", { name: '››' }) // Not grabbing - easy fix
 
         for (var i = 1; i < 8; i++) {
             expect(pageNumber).toEqual(i)
             fireEvent.click(incrementPage)
+            pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
         }
         expect(pageNumber).toEqual(7)
     })
     it("Decrements and does not pass the seventh page", () => {
         render(<HourlyForecast title="Hourly" items={weather} />)
-        const pageNumber = screen.getByTestId("page").value
-        const incrementPage = screen.getByRole("button", { name: ">>" }) // Not grabbing - easy fix
-        const decrementPage = screen.getByRole("button", { name: "<<" }) // Not grabbing - easy fix
+        var pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
+        const incrementPage = screen.getByRole("button", { name: "››" })
+        const decrementPage = screen.getByRole("button", { name: "‹‹" })
 
         for (var i = 1; i < 7; i++) {
             fireEvent.click(incrementPage)
         }
-
+        pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
         for (var i = 7; i > 1; i--) {
             expect(pageNumber).toEqual(i)
             fireEvent.click(decrementPage)
+            pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
         }
         expect(pageNumber).toEqual(1)
     })
     it("Resets page number on weather data change", () => {
-        render(<HourlyForecast title="Hourly" items={weather} />)
-        const pageNumber = screen.getByTestId("page").value  // Not grabbing
-        const incrementPage = screen.getByRole("button", { name: ">>" })  // Not grabbing - easy fix
+        render(<HourlyForecast title = "Hourly" items = {weather} />)
+        var pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
+        const incrementPage = screen.getByRole("button", { name: "››" })
 
         expect(pageNumber).toEqual(1)
         fireEvent.click(incrementPage)
+        pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
         expect(pageNumber).toEqual(2)
 
         let londonWeather = null
         getFormattedWeatherData({ q: "London" }, "metric").then((data) => {
             londonWeather = data.hourly
             screen.HourlyForecast.items = londonWeather
+            pageNumber = Number(screen.getByTestId("page").getAttribute('value'))
+            expect(pageNumber).toEqual(1)
             // Could have a timing problem - got upset with having expect in this .then() promise catch
         })
-        expect(pageNumber).toEqual(1)
     })
 })
